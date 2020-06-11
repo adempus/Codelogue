224359d99574e89c66900eb8caa4b66183942080
adempus <jmorin722@gmail.com>
2020-06-11 19:54:03 -0400

    test commit

--- backend/src/routes/api.py
+++ backend/src/routes/api.py
@@ -15,22 +15,23 @@ def landingPage():
 
 @index.route('/sign-up', methods=['GET', 'POST'])
 def signUp():
-    return jsonify({"route_hit": "sign-up"})
+    return jsonify({'route_hit': 'sign-up'})
 
 
 @index.route('/sign-in', methods=['GET', 'POST'])
 def signIn():
     if request.method == 'POST':
         signInData = dict(request.get_json())
-        return jsonify({ 'signInData': signInData })
+        return jsonify({'signInData': signInData })
     else:
         return jsonify({'route_hit': 'sign-in'})
 
 
-''' user routes '''
+''' user routes. '''
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
 @user.route('/', methods=['GET', 'POST'])
 def getUser():
-    return jsonify({"message": "Hello User!"})
+    return jsonify({'message': 'Hello User!'})
+
